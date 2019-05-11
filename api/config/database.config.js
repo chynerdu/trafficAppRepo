d87b321867adb02db725c5caf1359a7d551c66c2
chinedu <chinedu@crenettechlabs.com>
2019-05-11 22:53:01 +0100

    new commit

--- api/config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
- url: 'mongodb://chinedu:klicky123@ds245523.mlab.com:45523/assessmentchinedu'
-}
--- traffiApp_ui/modules/footer-script.php
+++
@@ -1,4 +0,0 @@
-
-<script type="text/javascript" src="scripts/jquery.js"></script>
-<script type="text/javascript" src="scripts/plugins.js" async></script>
-<script type="text/javascript" src="scripts/custom.js" async></script>
